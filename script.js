d36834e1f222d377a5a5ef175f66a2d17db9064f
Taraansh <taraanshnandwani123@gmail.com>
2023-04-03 16:37:56 +0530

    ...

---
+++ script.js
@@ -0,0 +1,54 @@
+const addBtn = document.getElementById("add");
+const notes = JSON.parse(localStorage.getItem("notes"));
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
+addBtn.addEventListener("click", () => addNewNote());
+function addNewNote(text = "") {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `  
+   <div class="tools">  
+   <button class="edit">${localStorage.getItem('editButtonState') === 'true' ? 'Save' : 'Edit'}</button>
+   <button class="delete">Delete</button>  
+   </div>  
+   <p class="main ${text ? "" : "hidden"}"></p>  
+   <textarea class="${text ? "hidden" : ""}" rows=8></textarea>  
+   `;
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+  textArea.value = text;
+  main.innerHTML = marked(text);
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+    updateLS();
+  });
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+    if (editBtn.innerText === 'Edit') {
+      editBtn.innerText = 'Save';
+      localStorage.setItem('editButtonState', true);
+    } else {
+      editBtn.innerText = 'Edit';
+      localStorage.setItem('editButtonState', false);
+      main.innerHTML = marked(textArea.value);
+      updateLS();
+    }
+  });
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
+    updateLS();
+  });
+  const notesList = document.getElementById("notes-list");
+  notesList.appendChild(note);
+}
+function updateLS() {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
